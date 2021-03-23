e809193025d9d803f4976404f30de5deb690bb01
Burlyn Andall-Blake <burlyn.andall-blake@oracle.com>
2021-03-23 13:16:28 -0400

    add print function for board

--- boardGame.py
+++ boardGame.py
@@ -5,14 +5,27 @@ class Board:
 
     def __init__(self):
         self.orgTable = []
+        self.curTable = []
         self.goalTable = []
 
     def makeBoard(self,filename):
         continue
-    def printBoard(self):
-        continue
+
+    def printBoard(self,letter):
+        if letter == "o" or letter == "O":
+            board=self.orgTable
+        elif letter == '"c" or letter == "C":
+            board=self.curTable
+        else:
+            board = self.goalTable
+
+        for ele in board:
+            print(*ele)
+    
+
     def makeMove(self):
         continue
+
     def heuisticAlgo(self):
         continue
 
