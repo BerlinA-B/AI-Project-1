ed341ae2104007f42cc5ea5455f77189a786b4b0
Burlyn Andall-Blake <burlyn.andall-blake@oracle.com>
2021-03-23 19:47:39 -0400

    added board creation function

--- boardGame.py
+++ boardGame.py
@@ -5,11 +5,22 @@ class Board:
 
     def __init__(self):
         self.orgTable = []
-        self.curTable = []
+        self.curTable = [[],[],[],[]]
         self.goalTable = []
 
     def makeBoard(self,filename):
-        continue
+        count = 1
+        for line in open(filename,'r',encoding="utf-8"):
+            '''up to an certain # of lines, make initial board'''
+            if count >= 1 and count<= 4:
+                st = line.split()
+                (self.orgTable).append(st)
+                
+            '''up to an certain # of lines, make goal board'''
+            elif count >= 6 and count<= 9:
+                st = line.split()
+                (self.goalTable).append(st)
+            count += 1 
 
     def printBoard(self,letter):
         if letter == "o" or letter == "O":
@@ -22,11 +33,11 @@ class Board:
         for ele in board:
             print(*ele)
     
-
+    
     def makeMove(self):
         continue
 
-    def heuisticAlgo(self):
+    def heuristicAlgo(self):
         continue
 
     
