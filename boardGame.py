8569c43d205c630a62d3a34a0e807be66b5514c8
Burlyn Andall-Blake <burlyn.andall-blake@oracle.com>
2021-03-23 11:18:28 -0400

    add framework for board class

--- boardGame.py
+++ boardGame.py
@@ -1,2 +1,19 @@
 import pandas as pd
 import numpy as np
+
+class Board:
+
+    def __init__(self):
+        self.orgTable = []
+        self.goalTable = []
+
+    def makeBoard(self,filename):
+        continue
+    def printBoard(self):
+        continue
+    def makeMove(self):
+        continue
+    def heuisticAlgo(self):
+        continue
+
+    
