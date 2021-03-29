95a33ffcaeed807754d87fc9f66a4fc8f534e73d
iwillseeyouinabits <isaaclsinger99@gmail.com>
2021-03-29 11:51:07 -0400

    clean up main

--- main.py
+++ main.py
@@ -4,16 +4,6 @@
 from AStar import AStar
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # board = Board()
-    # board.makeBoard("C:\\Users\\isaac\\Desktop\\AI Project 1\\AI-Project-1\\Sample_Input.txt")
-    # print("...")
-    # board.printBoard()
-    # print(":::")
-    # board.moveUp()
-    # print("<<<")
-    # board.printBoard()
-    # print("---")
-    # print(board.heuristicAlgo())
     search = AStar("C:\\Users\\isaac\\Desktop\\AI Project 1\\AI-Project-1\\Sample_Input.txt")
     search.aStar()
 
